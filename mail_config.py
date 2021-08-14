abc158cb80f857290745665f5c6ca138816695de
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-15 01:20:32 +0200

    Add Project Backend Set-up

---
+++ mail_config.py
@@ -0,0 +1,35 @@
+import smtplib
+# from email.MIMEImage import MIMEImage
+# from email.MIMEMultipart import MIMEMultipart
+# from email.MIMEText import MIMEText
+
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+
+def send_email(image, config):
+    msg_root = MIMEMultipart('related')
+    msg_root['Subject'] = 'Security Update'
+    msg_root['From'] = config.sender_email_address
+    msg_root['To'] = config.receiver_email_address
+    msg_root.preamble = 'Raspberry pi security camera update'
+
+    msg_alternative = MIMEMultipart('alternative')
+    msg_root.attach(msg_alternative)
+    msg_text = MIMEText('Smart security cam found object')
+    msg_alternative.attach(msg_text)
+
+    msg_text = MIMEText('<img src="cid:image1">', 'html')
+    msg_alternative.attach(msg_text)
+
+    msg_image = MIMEImage(image)
+    msg_image.add_header('Content-ID', '<image1>')
+    msg_root.attach(msg_image)
+
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.starttls()
+    smtp.login(config.sender_email_address, config.sender_email_password)
+    smtp.sendmail(config.sender_email_address, config.receiver_email_address, msg_root.as_string())
+    smtp.quit()
